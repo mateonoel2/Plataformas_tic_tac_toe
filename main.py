e0f23e3a275d5454bc943d624048f4826a739c46
mateonoel2 <mateo.noel@utec.edu.pe>
2023-05-10 01:32:30 -0500

    login created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from flask import Flask, jsonify,  request, render_template, redirect
+from flask import Flask, jsonify, request, render_template, redirect, flash
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,40 +8,46 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.secret_key = 'my_secret_key'
+
 db = SQLAlchemy(app)
 
 @dataclass
 class Player(db.Model):
     id: int
-    firstname: str
-    lastname: str
+    username: str
+    password: str
 
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(100), nullable=False)
-    lastname = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     
 
     def __repr__(self):
-        return f'<Player {self.firstname}>'
+        return f'<Player {self.username}>'
     
-def test_connection():
-    with app.app_context():
-        db.create_all()
-        player_test = Player(firstname='john', lastname='doe')
+    def check_password(self, password):
+        return self.password == password
+    
 
-        db.session.add(player_test)
-        db.session.commit()
-
-        students = Player.query.all()
-
-        print(students)
-        print(jsonify(students))
-
-test_connection()
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def menu():
     return render_template('menu.html')
+
+@app.route('/SignUp')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/LogIn')
+def login():
+    return render_template('login.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
 
 @app.route('/players', methods=['GET'])
 def route_get_players():
@@ -52,9 +59,9 @@ def route_get_player(player_id):
 
 @app.route('/players/add',  methods = ['POST'])
 def route_add_player():
-    firstname = request.form['firstname']
-    lastname = request.form['lastname']
-    player = Player(firstname=firstname, lastname=lastname)
+    username = request.form['username']
+    password = request.form['password']
+    player = Player(username=username, password=password)
     db.session.add(player)
     db.session.commit()
     return redirect('/')
@@ -69,6 +76,17 @@ def route_delete_player():
     db.session.delete(player)
     db.session.commit()
     return redirect('/')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login2():
+    username = request.form['username']
+    password = request.form['password']
+    player = Player.query.filter_by(username=username).first()
+    if player and player.check_password(password):
+        return redirect('/game')
+    else:
+        flash('Invalid username or password')
+        return redirect('/LogIn')
 
 @app.route('/players/delete/<player_id>', methods=['GET','DELETE'])
 def route_delete_player2(player_id):
@@ -87,8 +105,8 @@ def get_player_by_id(player_id):
     player = Player.query.get(player_id)
     return jsonify(player)
 
-def insert_player(firstname, lastname):
-    new_player = Player(name=firstname, age=lastname)
+def insert_player(username, password):
+    new_player = Player(name=username, age=password)
     db.session.add(new_player)
     db.session.commit()
     return new_player.id
